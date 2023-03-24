1df724ec98950cc5b9e95faf1e37c6f7527ef90e
Progga Roy~ <proggaroy2000@gmail.com>
2023-03-24 08:49:52 -0700

    litte bit explore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+// const number =202003312;
+const singer ={name :'Mahafuzur Rahman', job: 'singer'}
+const singer2 ={name :'Eva Rahman', job: 'singer'}
+const singerStyle ={
+  backgroundColor :'lightBlue',
+  color : 'blue'
+}
+const singerStyle2 ={
+  backgroundColor :'lightPink',
+
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="countries">
+      <h1>Hlw React Dude</h1>
+      <p style={singerStyle} className='music'>Name: {singer.name} <br />  Profession: {singer.job} </p>
+      <p  style={singerStyle2} className='music'>Name: {singer2.name} <br />  Profession: {singer2.job} </p>
+    </div>
     </div>
   );
 }
