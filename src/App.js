a1a71a629dc774cf0af1ada7a51140c23861068e
Progga Roy~ <proggaroy2000@gmail.com>
2023-03-25 07:41:20 -0700

    useState and useEffect introsuction and practice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
@@ -21,22 +23,72 @@ function App() {
   <Person name="Hrittik Rooshan" profession="Actor"></Person>
   <Person2 name="Deepika Padukone" profession="Actress"></Person2>
    */
-   products.map(product=> <Product name={product.name} price={product.price}></Product>)
+  //  products.map(product=> <Product name={product.name} price={product.price}></Product>)
    }
    {/* <Product name='Laptop' price="61000"></Product>
    <Product name='Xiaomi' price="30000"></Product>
    <Product name='Watch' price="4500"></Product>
    <Product name='Sunglass' price="300"></Product> */}
+<Count></Count>
+<ExternalUsers></ExternalUsers>
     </div>
   );
 }
 
-function Product(props){
-  return <div className='product'>
-<h3>Name :{props.name} </h3>
-<p>Price : {props.price} tk</p>
-  </div>
+function ExternalUsers(){
+  const [users,setUsers]= useState([]);
+  useEffect(()=>{
+ fetch('https://jsonplaceholder.typicode.com/users')
+ .then(res=>res.json())
+ .then(data=>setUsers(data))
+
+  },[])
+  return (
+    <div>
+    <h1>External user</h1>
+    {
+      users.map(user=><User name={user.name} email={user.email}></User>)
+    }
+    </div>
+  )
 }
+
+function User(props){
+return (
+  <div>
+      <h4>Name : {props.name}</h4>
+  <p>Email : {props.email}</p>
+  </div>
+)
+}
+
+
+function Count(){
+  const [count,setCount]= useState(33)
+  const increaseCount =()=>{
+    const newCount = count +1;
+   setCount(newCount)
+  }
+  const decreaseCount =()=>{
+    const newCount = count -1;
+   setCount(newCount)
+  }
+  return (
+    <div>
+      <h1>Count : {count}</h1>
+     <button onClick={increaseCount}>Increase</button>
+     <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+
+// function Product(props){
+//   return <div className='product'>
+// <h3>Name :{props.name} </h3>
+// <p>Price : {props.price} tk</p>
+//   </div>
+// }
 
 
 
