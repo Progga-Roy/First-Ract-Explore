3032134bbc997b2c60df50704fdcdebd234787dc
Progga Roy~ <proggaroy2000@gmail.com>
2023-03-25 01:32:12 -0700

    basic component create

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-// const number =202003312;
-const singer ={name :'Mahafuzur Rahman', job: 'singer'}
-const singer2 ={name :'Eva Rahman', job: 'singer'}
-const singerStyle ={
-  backgroundColor :'lightBlue',
-  color : 'blue'
-}
-const singerStyle2 ={
-  backgroundColor :'lightPink',
-
-
-}
 
 function App() {
   return (
     <div className="App">
-    <div className="countries">
-      <h1>Hlw React Dude</h1>
-      <p style={singerStyle} className='music'>Name: {singer.name} <br />  Profession: {singer.job} </p>
-      <p  style={singerStyle2} className='music'>Name: {singer2.name} <br />  Profession: {singer2.job} </p>
-    </div>
+  <Person></Person>
+  <Person2></Person2>
+  <Person></Person>
+  <Person2></Person2>
     </div>
   );
+}
+function Person(){
+  return (
+    <div className='person'>
+       <h1>Shah Rukh Khan</h1>
+       <p>Profession : Actor</p>
+    </div>
+  )
+  
+}
+function Person2() {
+  return (
+    <div className='person2'>
+       <h1>Hrittik Roshan</h1>
+       <p>Profession : Actor</p>
+    </div>
+  )
+
 }
 
 export default App;
